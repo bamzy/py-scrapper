22a0f9db84f16bfbafb68fc20017ffc1857d1e2b
Bamdad <aghilide@ualberta.ca>
2020-04-20 19:07:50 -0400

    some more testing

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import random
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "helloworld"
+
+print(c.hump(txt))
+x, y, z = "Orange", "Banana", "Cherry"
+print(x,y,z)
+
+x = "awesome"
+
+
+
+print(random.randrange(1,100))
+
+x = "3"
+print(type(int(x)))
